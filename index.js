2fb5c7f39e7acebccf0e44091133b854a28b5d08
Divya-Tankala <125850669+Divya-Tankala@users.noreply.github.com>
2023-09-29 11:47:40 +0530

    index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function validateEmail() {
     // Check if the email matches the pattern
     if (emailPattern.test(email)) {
         // Email is valid
-        document.getElementById('email').textContent = "";
+        document.getElementById('validationResult').textContent = "";
     } else {
         // Email is not valid
-        document.getElementById('email').textContent = "Email is not valid.";
+        document.getElementById('validationResult').textContent = "Email is not valid.";
     }
 }
 function formatDate(inputDate) {
